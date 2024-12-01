bcc1ccc975c6046611ab47c51f702a5cffa84478
VersionControlAdmin <sebastian.luft.gm@gmail.com>
2024-12-01 13:07:21 +0100

    MVP Deployment Bug Fix 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignUp";
 import PrivateNotesHomePage from "./pages/PrivateNotesPage";
--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React from "react";
 import { motion } from "framer-motion";
 import { Sparkles } from "lucide-react";
 import { useNavigate } from "react-router-dom";
